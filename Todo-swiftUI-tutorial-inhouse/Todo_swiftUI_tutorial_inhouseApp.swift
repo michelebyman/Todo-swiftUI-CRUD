5fad73b9567def3b96c2f31ef5c331f34eca109d
Michele Byman <michelebyman@gmail.com>
2020-11-24 14:02:28 +0100

    Initial Commit

---
+++ Todo-swiftUI-tutorial-inhouse/Todo_swiftUI_tutorial_inhouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_swiftUI_tutorial_inhouseApp.swift
+//  Todo-swiftUI-tutorial-inhouse
+//
+//  Created by Michele Byman on 2020-11-24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_swiftUI_tutorial_inhouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
